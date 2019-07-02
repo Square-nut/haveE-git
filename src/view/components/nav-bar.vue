942271b98514979a3de2d747996e0ae56c03c903
liruiqing <liruiqing@300.cn>
2019-07-02 16:10:03 +0800

    update
    Signed-off-by: liruiqing <liruiqing@300.cn>

---
+++ src/view/components/nav-bar.vue
@@ -0,0 +1,286 @@
+<template>
+  <div class="header">
+    <div class="header-con">
+      <a href=""
+         class="logo"></a>
+      <div class="logo_rt">
+        <a href="">
+          <h1>有翼云</h1>
+          <p>助力企业打造自有生态链</p>
+        </a>
+      </div>
+      <ul class="nav">
+        <li class="active"><a href=""
+             class="product-center">首页</a></li>
+        <li class="product-dropdown"><a href="javascript:;"
+             class="product-center">商家服务<i class="caret"></i></a>
+          <div class="dropdown-menu-wrapper">
+            <div class="dropdown-menu">
+              <div class="item-container">
+                <div class="item-product"><a href=""
+                     class="item-product-link"><span class="icon-xls"></span>
+                    <h3>新零售</h3>
+                    <p>商城丨分销丨营销丨会员丨多商户管理</p>
+                  </a></div>
+                <div class="item-product"><a href=""
+                     class="item-product-link"><span class="icon-xfw"></span>
+                    <h3>新服务</h3>
+                    <p>店铺丨会员丨营销丨外卖丨预约丨支付</p>
+                  </a></div>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li><a href=""
+             class="product-center">渠道合作</a></li>
+        <li><a href=""
+             class="product-center">关于有翼</a></li>
+        <li class="last login_common"><a target="_blank"
+             class="btn-login"
+             href="">登录</a>
+        </li>
+        <li class=" last register_common"><a target="_blank"
+             href="/register.html">注册</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+<style scoped>
+/*header*/
+
+.header {
+  width: 100%;
+  min-width: 1200px;
+  height: auto;
+  position: relative;
+  background: #ffffff;
+  border-bottom: 1px solid #ededed;
+  box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.1);
+  z-index: 200;
+  transition: all 0.5s ease-in;
+}
+
+.header-con {
+  box-sizing: border-box;
+  margin: 0 auto;
+  width: 1200px;
+  height: 60px;
+  zoom: 1;
+}
+
+.logo {
+  display: inline-block;
+  width: 74px;
+  height: 34px;
+  margin-top: 15px;
+  background-size: 74px 34px;
+  background-image: url(/static/assets/logo.png);
+}
+
+.logo_rt {
+  display: inline-block;
+  width: 160px;
+  margin-left: 15px;
+}
+
+.logo_rt h1 {
+  color: #1d2c39;
+  font-size: 20px;
+  font-family: "黑体";
+  line-height: 20px;
+}
+
+.logo_rt p {
+  color: #a2a2a2;
+  font-size: 12px;
+}
+
+.nav {
+  float: right;
+  zoom: 1;
+}
+
+.nav > li {
+  float: left;
+  display: inline-block;
+  height: 60px;
+  line-height: 60px;
+  padding: 0px 30px;
+}
+
+.nav > li .product-center {
+  font-size: 14px;
+}
+
+.nav > li:hover .product-center {
+  color: #0092dc;
+  cursor: pointer;
+}
+
+.nav .caret {
+  background-image: url(/static/assets/icons.png);
+  background-repeat: no-repeat;
+  background-size: auto;
+}
+
+.nav li.active a {
+  color: #0092dc;
+}
+
+.nav .product-dropdown .caret {
+  position: relative;
+  display: inline-block;
+  width: 14px;
+  height: 10px;
+  background-position: 0 0;
+  margin-left: 5px;
+}
+
+.nav .product-dropdown:hover .caret {
+  background-position: 0 -17px;
+}
+
+.nav .product-dropdown .dropdown-menu-wrapper {
+  position: absolute;
+  left: 0;
+  border-bottom: 0.5px solid #e5e5e5;
+  transform: rotate3d(1, 0, 0, -90deg);
+  z-index: 100;
+  max-height: 0px;
+  box-shadow: inset 0px 2px 5px rgba(0, 0, 0, 0.07);
+  transform-origin: 50% 0%;
+  transition: 300ms ease-in-out;
+  overflow: hidden;
+  background-color: #fff;
+  width: 100%;
+}
+
+.nav .dropdown-menu-wrapper .dropdown-menu {
+  margin: 30px auto;
+  width: 100%;
+  max-width: 800px;
+  zoom: 1;
+  overflow: hidden;
+}
+
+.nav
+  .dropdown-menu-wrapper
+  .dropdown-menu.dropdown-menu__customer
+  .item-container {
+  width: 950px;
+}
+
+.nav .dropdown-menu-wrapper .dropdown-menu .item-product {
+  width: 400px;
+  box-sizing: border-box;
+  height: 80px;
+  float: left;
+}
+
+.nav .product-dropdown:hover .dropdown-menu-wrapper {
+  -webkit-transform: rotate3d(0, 0, 0, 0deg);
+  -moz-transform: rotate3d(0, 0, 0, 0deg);
+  transform: rotate3d(0, 0, 0, 0deg);
+  -webkit-transform-origin: 50% 0;
+  -moz-transform-origin: 50% 0;
+  -ms-transform-origin: 50% 0;
+  transform-origin: 50% 0;
+  max-height: 1000px;
+}
+
+.dropdown-menu-wrapper .dropdown-menu .item-container .item-product {
+  position: relative;
+}
+
+.product-dropdown .dropdown-menu-wrapper .dropdown-menu .item-product-link {
+  display: block;
+  width: 100%;
+  height: 100%;
+}
+
+.product-dropdown .dropdown-menu-wrapper .dropdown-menu .item-container span {
+  width: 23px;
+  height: 23px;
+  position: absolute;
+  left: 10px;
+  top: 16px;
+}
+
+.product-dropdown .dropdown-menu-wrapper .dropdown-menu .item-product-link h3 {
+  font-size: 18px;
+  line-height: 22px;
+  color: #333333;
+  padding: 16px 0px 23px 80px;
+}
+
+.product-dropdown .dropdown-menu-wrapper .dropdown-menu .item-product-link p {
+  font-size: 12px;
+  line-height: 12px;
+  color: #999999;
+  padding-left: 80px;
+}
+
+.product-dropdown
+  .dropdown-menu-wrapper
+  .dropdown-menu
+  .item-container
+  span.icon-xls {
+  width: 60px;
+  height: 60px;
+  background: url(/static/assets/lingshou.png);
+}
+
+.product-dropdown
+  .dropdown-menu-wrapper
+  .dropdown-menu
+  .item-container
+  span.icon-xfw {
+  width: 60px;
+  height: 60px;
+  background: url(/static/assets/fw.png);
+}
+
+.nav li.last {
+  padding: 0px 0px 0px 20px;
+}
+
+.nav li.login_common a {
+  width: 80px;
+  height: 30px;
+  line-height: 28px;
+  display: inline-block;
+  border: 1px solid #2b90ed;
+  color: #2b90ed;
+  text-align: center;
+  border-radius: 2px;
+  box-sizing: border-box;
+  cursor: pointer;
+}
+
+.nav li.login_common a:hover {
+  background-color: #2b90ed;
+  color: #fff;
+}
+
+.nav li.register_common a {
+  width: 80px;
+  height: 30px;
+  line-height: 30px;
+  display: inline-block;
+  background-color: #2b90ed;
+  color: #ffffff;
+  text-align: center;
+  border-radius: 2px;
+  cursor: pointer;
+}
+</style>
--- src/view/home.vue
+++ src/view/home.vue
@@ -1,30 +1,33 @@
 <template>
   <div>
+    <!--header-->
+    <Nav-bar></Nav-bar>
+    <!--header/-->
     <!--banner-->
     <div class="banner">
       <div class="swiper-container">
         <div class="swiper-wrapper">
           <div class="swiper-slide slide-1">
             <div class="swiper-bg"
-                 style="background-image:url('images/banner_01.png')"></div>
+                 style="background-image:url('/static/assets/banner_01.png')"></div>
             <div class="content-wrapper">
               <h3>有翼 新零售</h3>
               <p class="font-lighter">线上零售行业品牌电商、门店零售行业解决方案</p>
-              <router-link class="button primary try-free-wsc"
-                           to="/add-shop?dictItemParentCode=NRetail_Shop&skuId=B4C95698100A435C9B747E58CCF14FFE">免费试用</router-link>
+              <a class="button primary try-free-wsc"
+                 href="/add-shop.html?dictItemParentCode=NRetail_Shop&skuId=B4C95698100A435C9B747E58CCF14FFE">免费试用</a>
               <small class="font-lighter">或致电 010 - 1234 5678</small>
             </div>
           </div>
           <div class="swiper-slide slide-2">
             <div class="swiper-bg"
-                 style="background-image:url('images/banner_02.png')"></div>
+                 style="background-image:url('/static/assets/banner_02.png')"></div>
             <div class="content-wrapper">
               <h3>有翼新服务</h3>
               <p class="font-lighter x-title-bn">
                 <span>服务于广大实体行业，提供客户沉淀、提升复购、</span><br><span>低成本拓客的线下门店解决方案</span>
               </p>
               <a class="button primary try-free-linshou"
-                 to="/add-shop?dictItemParentCode=NSever_GEN&skuId=32C3F7EF28264A1A88607F357D880B19">免费试用</a>
+                 href="/add-shop.html?dictItemParentCode=NSever_GEN&skuId=32C3F7EF28264A1A88607F357D880B19">免费试用</a>
               <small class="font-lighter">或致电 010 - 1234 5678</small>
             </div>
           </div>
@@ -36,6 +39,26 @@
         </div>
       </div>
     </div>
+    <!-- <div class="banner">
+      <el-carousel trigger="click"
+                   class=""
+                   height="150px">
+        <el-carousel-item v-for="item in swiperList"
+                          :key="item.id">
+          <div class="swiper-slide slide-1"
+               :style="`background-image:url(${item.bgURL})`">
+            <div class="content-wrapper">
+              <h3>{{item.name}}</h3>
+              <p class="font-lighter"
+                 v-html="item.desc"></p>
+              <a class="button primary try-free-wsc"
+                 to="">免费试用</a>
+              <small class="font-lighter">或致电 010 - 1234 5678</small>
+            </div>
+          </div>
+        </el-carousel-item>
+      </el-carousel>
+    </div> -->
     <!--banner/-->
     <!--商业模式-->
     <div class="model-con">
@@ -44,7 +67,7 @@
           <p>线上——移动——线下</p>
           <p>三位一体全时空的体验店营销系统</p>
         </h3>
-        <img src="images/omo_online.png">
+        <img src="/static/assets/omo_online.png">
       </div>
     </div>
     <!--商业模式/-->
@@ -57,25 +80,25 @@
         <div class="wrapper">
           <ul>
             <li>
-              <img src="images/demand_01.png">
+              <img src="/static/assets/demand_01.png">
               <h6>智能化需求</h6>
               <p>传统商家的低成本</p>
               <p>互联网移动化升级</p>
             </li>
             <li>
-              <img src="images/demand_02.png">
+              <img src="/static/assets/demand_02.png">
               <h6>沉淀客户需求</h6>
               <p>促进客户持续购买力</p>
               <p>和扩大客户群体需求</p>
             </li>
             <li>
-              <img src="images/demand_03.png">
+              <img src="/static/assets/demand_03.png">
               <h6>降低成本需求</h6>
               <p>多门店管理、收款、</p>
               <p>提高效率需求</p>
             </li>
             <li>
-              <img src="images/demand_04.png">
+              <img src="/static/assets/demand_04.png">
               <h6>集中管理需求</h6>
               <p>集中管理需求</p>
             </li>
@@ -92,9 +115,9 @@
     <!--解决方案-->
     <div class="solution-con">
       <div class="wrapper">
-        <a href=""><img src="images/omo_project1.png"></a>
+        <a href=""><img src="/static/assets/omo_project1.png"></a>
         <a href=""
-           class="ml-8"><img src="images/omo_project2.png"></a>
+           class="ml-8"><img src="/static/assets/omo_project2.png"></a>
       </div>
     </div>
     <!--解决方案/-->
@@ -143,32 +166,32 @@
         <h3>全方位服务体系支撑</h3>
         <ul>
           <li>
-            <img src="images/omo_service1.png">
+            <img src="/static/assets/omo_service1.png">
             <h5>客服中心</h5>
             <p>7*12小时专属人工在线客服提供全方位客户支持</p>
           </li>
           <li>
-            <img src="images/omo_service2.png">
+            <img src="/static/assets/omo_service2.png">
             <h5>培训中心</h5>
             <p>每周1次线上、线下培训手把手教会使用产品</p>
           </li>
           <li>
-            <img src="images/omo_service3.png">
+            <img src="/static/assets/omo_service3.png">
             <h5>运营分享</h5>
             <p>每月实操运营案例、运营经验分享</p>
           </li>
           <li>
-            <img src="images/omo_service4.png">
+            <img src="/static/assets/omo_service4.png">
             <h5>企业论坛</h5>
             <p>企业信息交互，使用产品反馈运营操作分享</p>
           </li>
           <li>
-            <img src="images/omo_service5.png">
+            <img src="/static/assets/omo_service5.png">
             <h5>本地服务</h5>
             <p>全国72家直营分公司本地化服务</p>
           </li>
           <li>
-            <img src="images/omo_service6.png">
+            <img src="/static/assets/omo_service6.png">
             <h5>搭建指导</h5>
             <p>为个性化建立自有平台，提供技术支持</p>
           </li>
@@ -216,13 +239,187 @@
   </div>
 </template>
 <script>
+import Navbar from './components/nav-bar'
 export default {
+  components: {
+    Navbar
+  },
   data () {
     return {
-
+      swiperList: [{
+        id: 'swiper1',
+        name: '有翼 新零售',
+        bgURL: '/static/assets/banner_01.png',
+        desc: '线上零售行业品牌电商、门店零售行业解决方案',
+        url: '/add-shop?dictItemParentCode=NRetail_Shop&skuId=B4C95698100A435C9B747E58CCF14FFE'
+      }, {
+        id: 'swiper2',
+        name: '有翼新服务',
+        bgURL: '/static/assets/banner_02.png',
+        desc: '<span>服务于广大实体行业，提供客户沉淀、提升复购、</span><br><span>低成本拓客的线下门店解决方案</span>',
+        url: '/add-shop?dictItemParentCode=NSever_GEN&skuId=32C3F7EF28264A1A88607F357D880B19'
+      }
+      ]
     }
   }
 }
 </script>
 <style scoped>
+/*swiper*/
+
+.swiper-bg {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-size: cover;
+  background-position: 50%;
+}
+
+.content-wrapper {
+  width: 1200px;
+  height: 100%;
+  margin: auto;
+}
+h3 {
+  font-size: 44px;
+  margin: 0 0 7px;
+  line-height: 62px;
+}
+
+.swiper-container .slide-1 h3 {
+  color: #0191dc;
+}
+
+.swiper-container .slide-1 p {
+  font-size: 22px;
+  color: #3c3c3c;
+  margin-bottom: 55px;
+}
+
+.swiper-container a.button {
+  width: 192px;
+  height: 46px;
+  line-height: 18px;
+  display: inline-block;
+  text-align: center;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+}
+
+.swiper-container a.try-free-wsc {
+  line-height: 24px;
+}
+
+a.primary {
+  background: url(/static/assets/s_01.png);
+  padding: 12px 42px;
+  color: #fff;
+}
+
+a:hover.primary {
+  background: url(/static/assets/s_02.png);
+}
+
+a.button {
+  font-size: 18px;
+  padding: 12px 22px;
+  text-decoration: none;
+  border-radius: 2px;
+}
+
+.swiper-container .slide-1 small {
+  font-size: 14px;
+  color: #7e7e7e;
+  margin-top: 15px;
+  display: block;
+}
+
+.swiper-container .slide-2 h3 {
+  color: #fff;
+}
+
+.swiper-container .slide-2 p {
+  font-size: 22px;
+  color: #d5ecf9;
+  line-height: 32px;
+  margin-bottom: 55px;
+}
+
+.swiper-container .slide-2 small {
+  font-size: 14px;
+  color: #d5ecf9;
+  margin-top: 15px;
+  display: block;
+}
+
+.swiper-container a.try-free-linshou {
+  background: transparent;
+  color: #fff;
+  border: 1px solid #fff;
+  border-radius: 2px;
+  line-height: 18px;
+}
+
+.swiper-container a:hover.try-free-linshou {
+  background: #ffffff;
+  color: #0099cc;
+}
+
+.banner .swiper-pagination-bullet {
+  width: 60px;
+  height: 4px;
+  display: inline-block;
+  border-radius: initial;
+  background: #b5b5b5;
+  opacity: 1;
+}
+
+.banner .swiper-pagination-bullet-active {
+  opacity: 1;
+  background: #fff;
+}
+
+.banner .swiper-container .swiper-button-container {
+  width: 1200px;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  margin: auto;
+}
+
+.banner .swiper-button-next,
+.banner .swiper-button-prev {
+  position: absolute;
+  top: 50%;
+  width: 27px;
+  height: 44px;
+  margin-top: -22px;
+  z-index: 10;
+  cursor: pointer;
+  background-size: 27px 44px;
+  background-position: 50%;
+  background-repeat: no-repeat;
+}
+
+.banner .swiper-container .swiper-button-next {
+  right: 1220px;
+  left: auto;
+  width: 55px;
+  height: 55px;
+  background-size: 100% 100%;
+  background-image: url(/static/assets/lt.png);
+}
+
+.banner .swiper-container .swiper-button-prev {
+  left: 1220px;
+  right: auto;
+  width: 55px;
+  height: 55px;
+  background-size: 100% 100%;
+  background-image: url(/static/assets/rt.png);
+}
 </style>
